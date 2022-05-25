1de0a8537dff1643b2702809a4e6b3247452c1a0
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-25 07:46:31 -0300

    commit changes

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -10,16 +10,43 @@ class UserController{
 
 onSubmit(){
 
-    [...this.form.elements].forEach((element, index)=>{
+   addEventListener("submit", (e)=>{
 
-   console.log(element.name);
+       e.preventDefault();
+        this.getValues()
+   })
 
-    })
+    }
 
 
+message(user){
+
+   // console.log(user)
 }
+
 getValues(){
 
+      let user = {};
+    [...this.form.elements].forEach((element, index)=>{
+
+      if (["nome", "email", "celular","password"].indexOf(element.name) > -1 && !element.value){
+
+        element.classList.add("error")
+
+        //console.log(element)
+      }
+
+          if(element.name){
+        user[element.name] = element.value;
+
+  }
+        //console.log(element);
+        let object = new User(user.nome, user.email, user.celular, user.password);
+        this.message(object)
+})
+
+console.log(user);
 
 }
+
 }
---
+++ src/models/User.js
@@ -0,0 +1,14 @@
+class User{
+
+    constructor(nome, email, celular, password){
+
+        this.nome = nome;
+        this.email = email;
+        this.celular = celular;
+        this.password = password;
+
+
+
+    }
+    
+}
